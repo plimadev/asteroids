d3641982218edf944627617bcb8db17d47433d01
plima94 <pauloalima1994@gmail.com>
2025-02-15 20:23:01 +0000

    defined clock

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ def main():
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    gameClock = pygame.time.Clock()
+    dt = 0 
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             screen.fill('black')
             pygame.display.flip()
+            dt = gameClock.tick(60)/1000
+
+            
 
 if __name__ == "__main__":
     main()
