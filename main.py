7535f8f3f370cd91e6ddd59c4c9ba7263be34a88
plima94 <pauloalima1994@gmail.com>
2025-03-05 21:47:36 +0000

    adding collisions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -31,6 +32,11 @@ def main():
 
         updatable.update(dt)
 
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
 
         for obj in drawable:
@@ -44,3 +50,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
